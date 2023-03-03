b44f59462bfafac4823b4d07ce6e7e92500a2a8c
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-03-03 15:42:23 +0100

    Add expo-router with web support

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
+      require.resolve("expo-router/babel"),
+    ],
   };
 };
