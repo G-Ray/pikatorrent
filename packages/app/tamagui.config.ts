70ef6603f1b072c9b27392fa2f24177cd05b67ab
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-03-08 11:02:17 +0100

    Upgrade tamagui to 1.7.x

--- packages/app/tamagui.config.ts
+++ packages/app/tamagui.config.ts
@@ -1,36 +1,36 @@
-import { createInterFont } from "@tamagui/font-inter";
-import { createMedia } from "@tamagui/react-native-media-driver";
-import { shorthands } from "@tamagui/shorthands";
-import { themes, tokens } from "@tamagui/theme-base";
-import { createTamagui } from "tamagui";
-import { createAnimations } from "@tamagui/animations-react-native";
+import { createInterFont } from '@tamagui/font-inter'
+import { createMedia } from '@tamagui/react-native-media-driver'
+import { shorthands } from '@tamagui/shorthands'
+import { themes, tokens } from '@tamagui/themes'
+import { createTamagui } from 'tamagui'
+import { createAnimations } from '@tamagui/animations-react-native'
 
 const animations = createAnimations({
   bouncy: {
-    type: "spring",
+    type: 'spring',
     damping: 10,
     mass: 0.9,
     stiffness: 100,
   },
   lazy: {
-    type: "spring",
+    type: 'spring',
     damping: 20,
     stiffness: 60,
   },
   quick: {
-    type: "spring",
+    type: 'spring',
     damping: 20,
     mass: 1.2,
     stiffness: 250,
   },
-});
+})
 
-const headingFont = createInterFont();
-const bodyFont = createInterFont();
+const headingFont = createInterFont()
+const bodyFont = createInterFont()
 
 const config = createTamagui({
   animations,
-  defaultTheme: "dark",
+  defaultTheme: 'dark',
   shouldAddPrefersColorThemes: false,
   themeClassNameOnRoot: false,
   shorthands,
@@ -53,17 +53,17 @@ const config = createTamagui({
     gtLg: { minWidth: 1280 + 1 },
     short: { maxHeight: 820 },
     tall: { minHeight: 820 },
-    hoverNone: { hover: "none" },
-    pointerCoarse: { pointer: "coarse" },
+    hoverNone: { hover: 'none' },
+    pointerCoarse: { pointer: 'coarse' },
   }),
-});
+})
 
-export type AppConfig = typeof config;
+export type AppConfig = typeof config
 
-declare module "tamagui" {
+declare module 'tamagui' {
   // overrides TamaguiCustomConfig so your custom types
   // work everywhere you import `tamagui`
   interface TamaguiCustomConfig extends AppConfig {}
 }
 
-export default config;
+export default config
