9295d73af61041b1bc44b0860ffa923fc4e1a466
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-03-29 15:18:43 +0200

    Add settings screen with session info

--- packages/app/app/settings.tsx
+++ packages/app/app/settings.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from 'react-native'
+import { useContext, useEffect, useState } from 'react'
+import { TextArea } from 'tamagui'
+import { NodeContext } from '../contexts/node'
 
 export default function Settings() {
-  return (
-    <View>
-      <Text>Settings Screen</Text>
-    </View>
-  )
+  const { sendRPCMessage } = useContext(NodeContext)
+  const [session, setSession] = useState({})
+
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+        const response = await sendRPCMessage({
+          method: 'session-get',
+        })
+
+        setSession(response.payload.arguments)
+      } catch (e) {
+        console.log('catched error')
+      }
+    }
+
+    fetchSession()
+  }, [sendRPCMessage])
+
+  return <TextArea minHeight="100%" value={JSON.stringify(session, null, 2)} />
 }
