5a2826b72aaded308e736b31b577ec48a135156a
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-03-29 18:54:12 +0200

    Add sessions info + download dir form

--- packages/app/app/settings.tsx
+++ packages/app/app/settings.tsx
@@ -1,10 +1,14 @@
 import { useContext, useEffect, useState } from 'react'
-import { TextArea } from 'tamagui'
+import { Button, Form, Input, Label, Paragraph, XStack } from 'tamagui'
 import { NodeContext } from '../contexts/node'
+import { SessionsInfoDialog } from '../dialogs/SessionInfoDialog'
 
 export default function Settings() {
   const { sendRPCMessage } = useContext(NodeContext)
+  const [initialSession, setInitialSession] = useState({})
   const [session, setSession] = useState({})
+
+  const hasChanged = JSON.stringify(session) !== JSON.stringify(initialSession)
 
   useEffect(() => {
     const fetchSession = async () => {
@@ -13,14 +17,60 @@ export default function Settings() {
           method: 'session-get',
         })
 
+        setInitialSession(response.payload.arguments)
         setSession(response.payload.arguments)
       } catch (e) {
-        console.log('catched error')
+        console.log('error fetching session info', e)
       }
     }
 
     fetchSession()
   }, [sendRPCMessage])
 
-  return <TextArea minHeight="100%" value={JSON.stringify(session, null, 2)} />
+  const handleSubmit = async () => {
+    try {
+      const response = await sendRPCMessage({
+        method: 'session-set',
+        arguments: {
+          'download-dir': session['download-dir'],
+        },
+      })
+
+      setSession(response.payload.arguments)
+    } catch (e) {
+      console.log('error updating session info', e)
+    }
+  }
+
+  return (
+    <Form height="100%" space ai="flex-start" onSubmit={() => handleSubmit()}>
+      <XStack space w="100%">
+        <Label htmlFor="downloadDir">Download directory</Label>
+        <Input
+          flex={1}
+          id="downloadDir"
+          size="$4"
+          borderWidth={2}
+          value={session['download-dir']}
+          onChange={(e) => {
+            setSession((s) => ({ ...s, ['download-dir']: e.target.value }))
+          }}
+        />
+      </XStack>
+
+      <Form.Trigger asChild disabled={!hasChanged}>
+        <Button theme="blue" o={!hasChanged ? 0.5 : 1}>
+          Save
+        </Button>
+      </Form.Trigger>
+
+      <XStack space w="100%" ai="center">
+        <Label htmlFor="transmissionVersion">Transmission version</Label>
+        <Paragraph id="transmissionVersion">{session['version']}</Paragraph>
+        <SessionsInfoDialog session={session} />
+      </XStack>
+    </Form>
+  )
+
+  // return <TextArea minHeight="100%" value={JSON.stringify(session, null, 2)} />
 }
---
+++ packages/app/dialogs/SessionInfoDialog.tsx
@@ -0,0 +1,67 @@
+import { X } from '@tamagui/lucide-icons'
+import { Adapt, Button, Dialog, Sheet, TextArea, Unspaced } from 'tamagui'
+
+export const SessionsInfoDialog = ({ session }) => {
+  return (
+    <Dialog modal>
+      <Dialog.Trigger asChild>
+        <Button>Session info</Button>
+      </Dialog.Trigger>
+
+      <Adapt when="sm" platform="touch">
+        <Sheet zIndex={200000} modal dismissOnSnapToBottom>
+          <Sheet.Frame padding="$4" space>
+            <Adapt.Contents />
+          </Sheet.Frame>
+          <Sheet.Overlay />
+        </Sheet>
+      </Adapt>
+
+      <Dialog.Portal theme="dark">
+        <Dialog.Overlay
+          key="overlay"
+          animation="quick"
+          o={0.5}
+          enterStyle={{ o: 0 }}
+          exitStyle={{ o: 0 }}
+        />
+
+        <Dialog.Content
+          bordered
+          elevate
+          key="content"
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+          space
+          w="80%"
+          h="50%"
+        >
+          <Dialog.Title>Session info</Dialog.Title>
+
+          <TextArea f={1} value={JSON.stringify(session, null, 2)} />
+
+          <Unspaced>
+            <Dialog.Close asChild>
+              <Button
+                pos="absolute"
+                t="$3"
+                r="$3"
+                size="$2"
+                circular
+                icon={X}
+              />
+            </Dialog.Close>
+          </Unspaced>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
+  )
+}
