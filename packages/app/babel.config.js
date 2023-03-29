743fbff19220abcdd0dc35d41ab9310c1fa7999d
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-03-29 18:53:28 +0200

    format babel.config.js

--- packages/app/babel.config.js
+++ packages/app/babel.config.js
@@ -1,26 +1,26 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
     plugins: [
       // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
-      require.resolve("expo-router/babel"),
+      require.resolve('expo-router/babel'),
       [
-        "@tamagui/babel-plugin",
+        '@tamagui/babel-plugin',
         {
-          components: ["tamagui"],
-          config: "./tamagui.config.ts",
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
           logTimings: true,
         },
       ],
       [
-        "transform-inline-environment-variables",
+        'transform-inline-environment-variables',
         {
-          include: "TAMAGUI_TARGET",
+          include: ['TAMAGUI_TARGET', 'SIGNALING_URL'],
         },
       ],
-      "@babel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated/plugin",
+      '@babel/plugin-proposal-export-namespace-from',
+      'react-native-reanimated/plugin',
     ],
-  };
-};
+  }
+}
