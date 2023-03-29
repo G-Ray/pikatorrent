65276f2debe8cde44fe3a1c6e7f59c9e002f1f35
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-03-29 15:19:55 +0200

    Add RemoveTorrentDialog

---
+++ packages/app/dialogs/RemoveTorrentDialog.tsx
@@ -0,0 +1,91 @@
+import { Trash2, X } from '@tamagui/lucide-icons'
+import { useContext, useState } from 'react'
+import { Adapt, Button, Dialog, Sheet, Unspaced, XStack } from 'tamagui'
+import { NodeContext } from '../contexts/node'
+
+export const RemoveTorrentDialog = ({ id }) => {
+  const { sendRPCMessage } = useContext(NodeContext)
+
+  const handleRemoveTorrent = async (deleteLocalData: boolean = false) => {
+    try {
+      await sendRPCMessage({
+        method: 'torrent-remove',
+        arguments: { ids: [id], 'delete-local-data': deleteLocalData },
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  return (
+    <Dialog modal>
+      <Dialog.Trigger asChild>
+        <Button circular icon={Trash2} color="$red9"></Button>
+      </Dialog.Trigger>
+
+      <Adapt when="sm" platform="touch">
+        <Sheet zIndex={200000} modal dismissOnSnapToBottom>
+          <Sheet.Frame padding="$4" space>
+            <Adapt.Contents />
+          </Sheet.Frame>
+          <Sheet.Overlay />
+        </Sheet>
+      </Adapt>
+
+      <Dialog.Portal theme="dark">
+        <Dialog.Overlay
+          key="overlay"
+          animation="quick"
+          o={0.5}
+          enterStyle={{ o: 0 }}
+          exitStyle={{ o: 0 }}
+        />
+
+        <Dialog.Content
+          bordered
+          elevate
+          key="content"
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+          space
+        >
+          <Dialog.Title>Remove a torrent</Dialog.Title>
+
+          <XStack space="$4">
+            <Dialog.Close displayWhenAdapted asChild>
+              <Button onClick={() => handleRemoveTorrent(false)} theme="blue">
+                Remove torrent only
+              </Button>
+            </Dialog.Close>
+            <Dialog.Close displayWhenAdapted asChild>
+              <Button onClick={() => handleRemoveTorrent(true)} theme="yellow">
+                Remove torrent and data
+              </Button>
+            </Dialog.Close>
+          </XStack>
+
+          <Unspaced>
+            <Dialog.Close asChild>
+              <Button
+                pos="absolute"
+                t="$3"
+                r="$3"
+                size="$2"
+                circular
+                icon={X}
+              />
+            </Dialog.Close>
+          </Unspaced>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog>
+  )
+}
