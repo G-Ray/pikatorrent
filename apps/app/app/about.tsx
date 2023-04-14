18bd6da1cdd641f9accbc403259ee1d5ca9372fe
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-04-14 11:46:16 +0200

    display pikatorrent version in about

--- apps/app/app/about.tsx
+++ apps/app/app/about.tsx
@@ -3,11 +3,17 @@ import { Label, Paragraph, XStack, YStack } from 'tamagui'
 import { SessionsInfoDialog } from '../dialogs/SessionInfoDialog'
 import { useSession } from '../hooks/useSession'
 
+import { version } from '../package.json'
+
 export default function About() {
   const session = useSession()
 
   return (
     <YStack>
+      <XStack space w="100%" ai="center">
+        <Label htmlFor="transmissionVersion">PikaTorrent version</Label>
+        <Paragraph id="transmissionVersion">{version}</Paragraph>
+      </XStack>
       <XStack space w="100%" ai="center">
         <Label htmlFor="transmissionVersion">Transmission version</Label>
         <Paragraph id="transmissionVersion">{session['version']}</Paragraph>
