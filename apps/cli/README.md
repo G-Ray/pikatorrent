f122b86271809ab380a6e332eb22dab6bffc40ca
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-05-22 17:58:17 +0200

    cli: add README and emoji

---
+++ apps/cli/README.md
@@ -0,0 +1,12 @@
+# PikaTorrent cli
+
+🗲 A next-generation BitTorrent client 🗲
+
+## Usage
+
+```sh
+npm i -g pikatorrent
+pikatorrent node
+```
+
+Then click on the displayed secret link, or scan the qrcode to quickly add your node to [app.pikatorrent.com](https://app.pikatorrent.com).
--- apps/cli/cli.js
+++ apps/cli/cli.js
@@ -3,7 +3,9 @@ const { program } = require('commander')
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
-program.name('pikatorrent').description('A next-generation BitTorrent client')
+program
+  .name('pikatorrent')
+  .description('🗲 A next-generation BitTorrent client 🗲')
 
 program
   .command('node')
