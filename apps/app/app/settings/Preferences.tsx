fb7b5104a5a98af98a74b5b1d8ca9e5742a44864
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-05-24 17:22:03 +0200

    Add search engines management

--- apps/app/app/settings/Preferences.tsx
+++ apps/app/app/settings/Preferences.tsx
@@ -1,9 +1,21 @@
-import React, { useContext } from 'react'
-import { H2, Label, Separator, Switch, XStack, YStack, Button } from 'tamagui'
+import React, { useContext, useState } from 'react'
+import {
+  H2,
+  Label,
+  Separator,
+  Switch,
+  XStack,
+  YStack,
+  Button,
+  TextArea,
+  Paragraph,
+} from 'tamagui'
 import { SettingsContext } from '../../contexts/settings'
 import { Platform } from 'react-native'
 
 import { APP_URL } from '../../config'
+import defaultSettings from '../../defaultSettings.json'
+import { Undo } from '@tamagui/lucide-icons'
 
 const registerMagnetHandler = () => {
   if (Platform.OS === 'web') {
@@ -41,7 +53,7 @@ export const Preferences = () => {
 
   return (
     <YStack space ai="flex-start">
-      <H2>Preferences</H2>
+      <H2>App settings</H2>
       <XStack width={200} alignItems="center" space="$4">
         <Label
           paddingRight="$0"
@@ -74,6 +86,86 @@ export const Preferences = () => {
           </Button>
         </XStack>
       )}
+
+      <SearchEngines />
+    </YStack>
+  )
+}
+
+const SearchEngines = () => {
+  const { settings, updateSettings } = useContext(SettingsContext)
+  const [searchEnginesUrls, setSearchEnginesUrls] = useState(
+    settings.searchEnginesUrls || defaultSettings.searchEnginesUrls
+  )
+  const hasChanged =
+    JSON.stringify(searchEnginesUrls) !==
+    JSON.stringify(settings.searchEnginesUrls)
+
+  // Check every url is valid
+  const isValid = searchEnginesUrls.every((url: string) => {
+    try {
+      new URL(url)
+      return true
+    } catch (e) {
+      return false
+    }
+  })
+
+  const handleSave = () => {
+    updateSettings({
+      ...settings,
+      searchEnginesUrls,
+      selectedSearchEngineUrl: searchEnginesUrls[0],
+    })
+  }
+
+  const handleReset = () => {
+    setSearchEnginesUrls(defaultSettings.searchEnginesUrls)
+  }
+
+  return (
+    <YStack w="100%">
+      <XStack space="$4" w="100%">
+        <Label paddingRight="$0" minWidth={160}>
+          Search engines urls (one per line)
+        </Label>
+        <YStack f={1} w="100%">
+          <TextArea
+            theme="yellow"
+            w="100%"
+            height={200}
+            onChangeText={(text) => setSearchEnginesUrls(text.split('\n'))}
+            value={searchEnginesUrls.join('\n')}
+          />
+          {!isValid && <Paragraph color="$red9">Invalid urls</Paragraph>}
+        </YStack>
+      </XStack>
+      <XStack gap="$2">
+        <Button
+          icon={Undo}
+          onPress={handleReset}
+          disabled={
+            JSON.stringify(searchEnginesUrls) ===
+            JSON.stringify(defaultSettings.searchEnginesUrls)
+          }
+          o={
+            JSON.stringify(searchEnginesUrls) ===
+            JSON.stringify(defaultSettings.searchEnginesUrls)
+              ? 0.5
+              : 1
+          }
+        >
+          Reset
+        </Button>
+        <Button
+          theme="yellow"
+          onPress={handleSave}
+          disabled={!isValid || !hasChanged}
+          o={!isValid || !hasChanged ? 0.5 : 1}
+        >
+          Save
+        </Button>
+      </XStack>
     </YStack>
   )
 }
--- apps/app/components/SearchBar.tsx
+++ apps/app/components/SearchBar.tsx
@@ -1,16 +1,47 @@
 import { Search } from '@tamagui/lucide-icons'
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { Button, Input, XStack, Form } from 'tamagui'
+import { SearchEngineSelector } from './SearchEngineSelector'
+import { SettingsContext } from '../contexts/settings'
+
+import defaultSettings from '../defaultSettings.json'
+
+export type Engine = {
+  name: string
+  searchUrl: string
+  iconUri: string
+}
 
 export const SearchBar = () => {
   const [query, setQuery] = useState('')
+  const { settings, updateSettings } = useContext(SettingsContext)
+  const selectedSearchEngineUrl =
+    settings.selectedSearchEngineUrl || defaultSettings.searchEnginesUrls[0]
+
+  const engines: Array<Engine> = (
+    settings.searchEnginesUrls || defaultSettings.searchEnginesUrls
+  ).map((engineUrl: string) => {
+    const url = new URL(engineUrl)
+    const hostnameParts = url.hostname.split('.')
+
+    return {
+      name: hostnameParts[hostnameParts.length - 2], // ignore tld
+      searchUrl: engineUrl,
+      iconUrl: url.origin + '/favicon.ico',
+    }
+  })
+
+  const handleEngineChange = (name: string) => {
+    const engine = engines.find((e) => e.name === name)
+    if (engine) {
+      updateSettings({ ...settings, selectedSearchEngineUrl: engine.searchUrl })
+    }
+  }
 
   return (
     <Form
       maxWidth={600}
-      onSubmit={() =>
-        window.open(`https://www.google.com/search?q=${query}+torrent`)
-      }
+      onSubmit={() => window.open(`${selectedSearchEngineUrl}${query}`)}
       f={1}
     >
       <XStack>
@@ -28,6 +59,11 @@ export const SearchBar = () => {
           value={query}
           onChangeText={setQuery}
           fontSize={'$6'}
+        />
+        <SearchEngineSelector
+          engines={engines}
+          value={selectedSearchEngineUrl}
+          onValueChange={handleEngineChange}
         />
         <Form.Trigger asChild disabled={query.length === 0}>
           <Button
