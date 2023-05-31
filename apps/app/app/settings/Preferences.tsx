57dd70a83eeb3b8a9cc9836312024153216d0e6d
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-05-31 15:32:33 +0200

    Remove magnet registering for now

--- apps/app/app/settings/Preferences.tsx
+++ apps/app/app/settings/Preferences.tsx
@@ -1,35 +1,9 @@
 import React, { useContext } from 'react'
-import { H2, Label, Separator, Switch, XStack, YStack, Button } from 'tamagui'
+import { H2, Label, Separator, Switch, XStack, YStack } from 'tamagui'
 import { SettingsContext } from '../../contexts/settings'
-import { Platform } from 'react-native'
 
-import { APP_URL } from '../../config'
 import { SearchEngines } from './AppSettings/SearchEngines'
 import { TorrentCardInfo } from './AppSettings/TorrentCardInfo'
-
-const registerMagnetHandler = () => {
-  if (Platform.OS === 'web') {
-    // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/registerProtocolHandler
-
-    try {
-      if (window.navigator.unregisterProtocolHandler) {
-        // chromium browsers allow unregistering
-        window.navigator.unregisterProtocolHandler(
-          'magnet',
-          `${APP_URL}/?magnet=%s`
-        )
-      }
-
-      window.navigator.registerProtocolHandler(
-        'magnet',
-        `${APP_URL}/?magnet=%s`,
-        'PikaTorrent'
-      )
-    } catch (e) {
-      console.error(e)
-    }
-  }
-}
 
 export const Preferences = () => {
   const { settings, updateSettings } = useContext(SettingsContext)
@@ -62,20 +36,6 @@ export const Preferences = () => {
           <Switch.Thumb animation="quick" />
         </Switch>
       </XStack>
-      {Platform.OS === 'web' && (
-        <XStack ai="center" space="$4">
-          <Label htmlFor="register-magnet-button">
-            Allow pikatorrent to open magnet links in your browser:
-          </Label>
-          <Button
-            id="register-magnet-button"
-            theme="yellow"
-            onPress={() => registerMagnetHandler()}
-          >
-            Register
-          </Button>
-        </XStack>
-      )}
 
       <SearchEngines />
       <TorrentCardInfo />
