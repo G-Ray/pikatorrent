64fb32f708bda537bae22564bb626fb6d6d95ca3
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-07-11 15:42:53 +0200

    Improve about layout

--- apps/app/app/about.tsx
+++ apps/app/app/about.tsx
@@ -1,28 +1,35 @@
 import React from 'react'
-import { Label, Paragraph, XStack, YStack } from 'tamagui'
+import { Label, Paragraph, XStack, YStack, useMedia } from 'tamagui'
 import { SessionsInfoDialog } from '../dialogs/SessionInfoDialog'
 import { useSession } from '../hooks/useSession'
 
 import { version } from '../package.json'
 import { Platform } from 'react-native'
 import { DESKTOP_MAX_CONTENT_WIDTH } from '../constants/layout'
+import { SettingLayout } from '../components/SettingLayout'
 
 export default function About() {
   const { session } = useSession()
+  const media = useMedia()
 
   return (
-    <YStack maxWidth={DESKTOP_MAX_CONTENT_WIDTH}>
-      <XStack space w="100%" {...(Platform.OS === 'web' && { ai: 'center' })}>
-        <Label htmlFor="pikaTorrentVersion">PikaTorrent version</Label>
-        <Paragraph id="pikaTorrentVersion">{version}</Paragraph>
-      </XStack>
-      <XStack space w="100%" {...(Platform.OS === 'web' && { ai: 'center' })}>
-        <Label htmlFor="transmissionVersion">Transmission version</Label>
-        <Paragraph id="transmissionVersion">{session['version']}</Paragraph>
-      </XStack>
-      <XStack ml="auto">
-        <SessionsInfoDialog session={session} />
-      </XStack>
-    </YStack>
+    <XStack f={1} jc="center" w="100%">
+      <YStack
+        flexShrink={1}
+        {...(media.gtXs && { w: DESKTOP_MAX_CONTENT_WIDTH })}
+      >
+        <SettingLayout>
+          <Label htmlFor="pikaTorrentVersion">PikaTorrent version</Label>
+          <Paragraph id="pikaTorrentVersion">{version}</Paragraph>
+        </SettingLayout>
+        <SettingLayout>
+          <Label htmlFor="transmissionVersion">Transmission version</Label>
+          <Paragraph id="transmissionVersion">{session['version']}</Paragraph>
+        </SettingLayout>
+        <XStack ml="auto">
+          <SessionsInfoDialog session={session} />
+        </XStack>
+      </YStack>
+    </XStack>
   )
 }
