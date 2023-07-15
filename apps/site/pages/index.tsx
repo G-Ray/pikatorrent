f4da4abf4d8dfa8d9a9b30cf10dae53e035f0c07
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-07-15 14:15:06 +0200

    site: restore google play link

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -96,6 +96,13 @@ export default function Home() {
                   </Button>
                 </Link>
               </XStack>
+              <a href="https://play.google.com/store/apps/details?id=com.gray.pikatorrent&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+                <img
+                  width={200}
+                  alt="Get it on Google Play"
+                  src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                />
+              </a>
             </YStack>
           </YStack>
 
