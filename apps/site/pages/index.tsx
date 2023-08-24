c77318d33d13352bbefbde121d8f0413fac1e244
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-08-25 00:06:59 +0200

    site: add features & free torrents links

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -2,14 +2,30 @@ import Head from 'next/head'
 import {
   Button,
   Card,
+  H2,
+  ListItem,
   Paragraph,
   Separator,
   Switch,
   Theme,
   XStack,
+  YGroup,
   YStack,
 } from 'tamagui'
-import { Download, ExternalLink, Moon, Sun } from '@tamagui/lucide-icons'
+import {
+  Download,
+  ExternalLink,
+  MonitorSmartphone,
+  Moon,
+  Sun,
+  Link as LinkIcon,
+  Magnet,
+  Zap,
+  Tags,
+  Filter,
+  Search,
+  Share2,
+} from '@tamagui/lucide-icons'
 import { useThemeSetting } from '@tamagui/next-theme'
 import { useEffect, useState } from 'react'
 
@@ -104,22 +120,24 @@ export default function Home() {
                 />
               </a>
 
-              <Link
-                href={'https://github.com/G-Ray/pikatorrent/releases'}
-                style={{ textDecoration: 'none' }}
-                target="_blank"
-              >
-                <Button ml="$2" mb="$6" size="$2" icon={ExternalLink}>
-                  All downloads options
-                </Button>
-              </Link>
+              <YStack ai="center">
+                <Link
+                  href={'https://github.com/G-Ray/pikatorrent/releases'}
+                  style={{ textDecoration: 'none' }}
+                  target="_blank"
+                >
+                  <Button ml="$2" mb="$6" size="$2" icon={ExternalLink}>
+                    All downloads options
+                  </Button>
+                </Link>
 
-              <Link href="https://discord.gg/6HxCV4aGdy" target="_blank">
-                <img
-                  height={24}
-                  src="https://img.shields.io/badge/Join_us_on_discord-gray?logo=discord"
-                />
-              </Link>
+                <Link href="https://discord.gg/6HxCV4aGdy" target="_blank">
+                  <img
+                    height={24}
+                    src="https://img.shields.io/badge/Join_us_on_discord-gray?logo=discord"
+                  />
+                </Link>
+              </YStack>
             </YStack>
           </YStack>
 
@@ -148,6 +166,126 @@ export default function Home() {
               </Card>
             </Theme>
             <Paragraph>Then follow the instructions.</Paragraph>
+          </YStack>
+          <YStack mt="$12" gap="$4">
+            <H2 fontWeight="bold" mb="$4" textAlign="center">
+              Features
+            </H2>
+            <XStack gap="$2">
+              <MonitorSmartphone color="$green9" />
+              <Paragraph>
+                Available on Windows, Linux, Android, Node.js & the Web!
+              </Paragraph>
+            </XStack>
+            <Separator />
+            <XStack gap="$2">
+              <Moon />
+              <Paragraph>A modern, simple UI, with dark mode support</Paragraph>
+            </XStack>
+            <Separator />
+            <XStack gap="$2">
+              <Share2 color="$blue9" />
+              <Paragraph>
+                Share your torrents with your contacts, or the world, with a
+                direct link
+              </Paragraph>
+            </XStack>
+            <Separator />
+            <XStack gap="$2">
+              <Zap color="$yellow9" />
+              <Paragraph>
+                Blazing fast download speeds, with low cpu & memory usage
+              </Paragraph>
+            </XStack>
+            <Separator />
+            <XStack gap="$2">
+              <Tags color="$orange9" />
+              <Paragraph>Categorize your torrents with labels</Paragraph>
+            </XStack>
+            <Separator />
+            <XStack gap="$2">
+              <Filter color="$purple9" />
+              <Paragraph>
+                Quickly filter your torrents with a fuzzy search & labels
+              </Paragraph>
+            </XStack>
+            <Separator />
+            <XStack gap="$2">
+              <Search color="$blue9" />
+              <Paragraph>
+                Edit your favorite search engines to quickly find torrents
+                online
+              </Paragraph>
+            </XStack>
+            <Separator />
+            <XStack gap="$2">
+              <LinkIcon color="$pink9" />
+              <Paragraph>
+                Link the Android app with the desktop app to control your
+                torrents remotely. No need to setup a server or register an
+                account!
+              </Paragraph>
+            </XStack>
+          </YStack>
+          <YStack mt="$14" gap="$4" ai="center">
+            <H2 fontWeight="bold" mb="$4">
+              Free torrents
+            </H2>
+            <Paragraph>Direct links examples to free movies</Paragraph>
+            <YGroup alignSelf="center" bordered width={240} size="$4">
+              <YGroup.Item>
+                <ListItem>
+                  <Link
+                    href={
+                      'https://app.pikatorrent.com/add#magnet:?xt=urn:btih:c9e15763f722f23e98a29decdfae341b98d53056&dn=Cosmos+Laundromat&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fcosmos-laundromat.torrent'
+                    }
+                    style={{ textDecoration: 'none' }}
+                    target="_blank"
+                  >
+                    <Button icon={Download}>Cosmos Laundromat</Button>
+                  </Link>
+                </ListItem>
+              </YGroup.Item>
+              <YGroup.Item>
+                <ListItem>
+                  <Link
+                    href={
+                      'https://app.pikatorrent.com/add#magnet:?xt=urn:btih:209c8226b299b308beaf2b9cd3fb49212dbd13ec&dn=Tears+of+Steel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Ftears-of-steel.torrent'
+                    }
+                    style={{ textDecoration: 'none' }}
+                    target="_blank"
+                  >
+                    <Button icon={Download}>Tears of Steel</Button>
+                  </Link>
+                </ListItem>
+              </YGroup.Item>
+              <YGroup.Item>
+                <ListItem>
+                  <Link
+                    href={
+                      'https://app.pikatorrent.com/add#magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent'
+                    }
+                    style={{ textDecoration: 'none' }}
+                    target="_blank"
+                  >
+                    <Button icon={Download}>Sintel</Button>
+                  </Link>
+                </ListItem>
+              </YGroup.Item>
+              <YGroup.Item>
+                <ListItem>
+                  <Link
+                    href={
+                      'https://app.pikatorrent.com/add#magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent'
+                    }
+                    style={{ textDecoration: 'none' }}
+                    target="_blank"
+                  >
+                    <Button icon={Download}>Big Buck Bunny</Button>
+                  </Link>
+                </ListItem>
+              </YGroup.Item>
+            </YGroup>
           </YStack>
         </YStack>
       </main>
