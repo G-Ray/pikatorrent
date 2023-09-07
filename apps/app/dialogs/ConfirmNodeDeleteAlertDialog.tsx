88415d97ed4ef353f3468ce6a755818e1f80284b
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-09-07 16:11:22 +0200

    refactor node deletion modal

--- apps/app/dialogs/ConfirmNodeDeleteAlertDialog.tsx
+++ apps/app/dialogs/ConfirmNodeDeleteAlertDialog.tsx
@@ -1,60 +1,28 @@
 import { Delete } from '@tamagui/lucide-icons'
 import React from 'react'
-import { AlertDialog, Button, XStack, YStack } from 'tamagui'
+import { Button, Paragraph, XStack } from 'tamagui'
+import { Dialog } from './Dialog'
 
+// TODO: i18n
 export function ConfirmNodeDeleteAlertDialog({ onConfirm }) {
   return (
-    <AlertDialog native>
-      <AlertDialog.Trigger asChild>
+    <Dialog
+      trigger={
         <Button icon={Delete} theme="red" size="$2">
           Delete
         </Button>
-      </AlertDialog.Trigger>
-
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay
-          key="overlay"
-          animation="quick"
-          opacity={0.5}
-          enterStyle={{ opacity: 0 }}
-          exitStyle={{ opacity: 0 }}
-        />
-        <AlertDialog.Content
-          bordered
-          elevate
-          key="content"
-          animation={[
-            'quick',
-            {
-              opacity: {
-                overshootClamping: true,
-              },
-            },
-          ]}
-          enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-          exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-          x={0}
-          scale={1}
-          opacity={1}
-          y={0}
-        >
-          <YStack space>
-            <AlertDialog.Title>Confirm node removal</AlertDialog.Title>
-            <AlertDialog.Description>
-              Are you sure you want to delete this node ?
-            </AlertDialog.Description>
-
-            <XStack space="$3" justifyContent="flex-end">
-              <AlertDialog.Cancel asChild>
-                <Button>Cancel</Button>
-              </AlertDialog.Cancel>
-              <AlertDialog.Action asChild onPress={onConfirm}>
-                <Button theme="red">Delete</Button>
-              </AlertDialog.Action>
-            </XStack>
-          </YStack>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
-    </AlertDialog>
+      }
+      title="Confirm node removal"
+    >
+      <Paragraph>Are you sure you want to delete this node ?</Paragraph>
+      <XStack space="$3" justifyContent="flex-end">
+        <Dialog.Close asChild>
+          <Button>Cancel</Button>
+        </Dialog.Close>
+        <Dialog.Close asChild onPress={onConfirm}>
+          <Button theme="red">Delete</Button>
+        </Dialog.Close>
+      </XStack>
+    </Dialog>
   )
 }
