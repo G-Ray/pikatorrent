6a317eed6deccf66cf8ed102c4af7b1ba30d2507
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-09-18 16:28:26 +0200

    SessionInfo: Fix display on desktop

--- apps/app/dialogs/SessionInfoDialog.tsx
+++ apps/app/dialogs/SessionInfoDialog.tsx
@@ -1,17 +1,45 @@
 import React from 'react'
-import { Button, Paragraph, ScrollView, TextArea } from 'tamagui'
+import {
+  Button,
+  Paragraph,
+  ScrollView,
+  Stack,
+  TextArea,
+  useThemeName,
+} from 'tamagui'
 import { Dialog } from './Dialog'
 import i18n from '../i18n'
+import { Platform } from 'react-native'
 
 export const SessionsInfoDialog = ({ session }) => {
+  const theme = useThemeName()
+
   return (
     <Dialog
       title="Session info"
-      trigger={<Button>{i18n.t('sessionInfoDialog.title')}</Button>}
+      trigger={
+        <Button
+          bc={theme.startsWith('light') ? 'white' : 'black'}
+          theme="yellow"
+          hoverTheme
+        >
+          {i18n.t('sessionInfoDialog.title')}
+        </Button>
+      }
       snapPoints={[90]}
     >
       <ScrollView>
-        <Paragraph>{JSON.stringify(session, null, 2)}</Paragraph>
+        {Platform.OS === 'web' ? (
+          <Stack minWidth={'40vw'} minHeight={'50vh'}>
+            <TextArea
+              w="100%"
+              h="100%"
+              value={JSON.stringify(session, null, 2)}
+            />
+          </Stack>
+        ) : (
+          <Paragraph>{JSON.stringify(session, null, 2)}</Paragraph>
+        )}
       </ScrollView>
     </Dialog>
   )
