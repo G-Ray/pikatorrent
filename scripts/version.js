95fe1d12a2d19ef17502db68949311c202506f0a
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-11-23 15:22:28 +0100

    version.js: increase android versionCode

--- scripts/version.js
+++ scripts/version.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const { spawnSync, execSync } = require('node:child_process')
 const { program } = require('commander')
+const fs = require('node:fs/promises')
+const path = require('node:path')
 
 const runCommand = async (command, args = []) => {
   spawnSync(command, args, { stdio: 'inherit' })
@@ -8,6 +10,27 @@ const runCommand = async (command, args = []) => {
 
 const setPkgVersionToWildcard = async (workspace, pkg) => {
   runCommand('npm', ['-w', workspace, 'pkg', 'set', `${pkg}=*`])
+}
+
+const increaseAndroidVersionCode = async () => {
+  const appConfigPath = path.join(__dirname, '../apps/app/app.config.js')
+  const fileContent = await fs.readFile(appConfigPath, { encoding: 'utf-8' })
+  const regex = new RegExp('^.*' + 'versionCode' + '.*$', 'gm')
+  const results = fileContent.match(regex)
+
+  if (!results) {
+    return
+  }
+
+  const versionCodeString = results[0].trim()
+  const [_, currentVersionString] = versionCodeString.split(':')
+  const nextVersion = parseInt(currentVersionString.slice(0, -1).trim()) + 1 // slice to remove comma
+  const newContent = fileContent.replace(
+    versionCodeString,
+    versionCodeString.replace(currentVersionString.trim(), nextVersion + ',')
+  )
+
+  await fs.writeFile(appConfigPath, newContent, { encoding: 'utf-8' })
 }
 
 const updateVersion = async (version) => {
@@ -28,6 +51,8 @@ const updateVersion = async (version) => {
 
   // npm does not commit or tag when using --ws
   runCommand('npm', ['version', '--ws', '--save', '--save-exact', version])
+
+  await increaseAndroidVersionCode()
 
   const newVersion = require('../apps/cli/package.json').version
 
