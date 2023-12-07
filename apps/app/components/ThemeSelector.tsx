ea447e77048e711c7cac858c6576b067caec051c
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-12-07 20:33:33 +0100

    Capitalize theme values

--- apps/app/components/ThemeSelector.tsx
+++ apps/app/components/ThemeSelector.tsx
@@ -24,7 +24,7 @@ export const ThemeSelector = () => {
       options={options}
       placeholder={'theme'}
       label={'Theme'}
-      optionsTexts={['System', 'light', 'dark']}
+      optionsTexts={['System', 'Light', 'Dark']}
     />
   )
 }
