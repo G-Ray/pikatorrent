2c4a2dee5722f6a271b1a2ccc78e6e4d0cc70b7f
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-12-12 12:17:14 +0100

    rm card around GlobalStats

--- apps/app/components/GlobalStats.tsx
+++ apps/app/components/GlobalStats.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Card, XStack, useMedia } from 'tamagui'
+import { XStack, useMedia } from 'tamagui'
 import { TorrentFieldFormatter } from './TorrentFieldFormatter'
 import { TorrentsContext } from '../contexts/TorrentsContext'
 
@@ -8,22 +8,19 @@ export const GlobalStats = () => {
   const media = useMedia()
 
   return (
-    <Card br={50} bc={'$backgroundTransparent'} py="$2">
-      <XStack gap={media.gtXs ? '$4' : '$4'}>
-        <TorrentFieldFormatter
-          fontSize={'$3'}
-          // fontWeight="bold"
-          name="rateDownload"
-          value={sessionStats.downloadSpeed || 0}
-        />
-        <TorrentFieldFormatter
-          fontSize={'$3'}
-          // fontWeight="bold"
-          name="rateUpload"
-          value={sessionStats.uploadSpeed || 0}
-        />
-      </XStack>
-      {/* </XStack> */}
-    </Card>
+    <XStack gap={media.gtXs ? '$4' : '$4'}>
+      <TorrentFieldFormatter
+        fontSize={'$3'}
+        // fontWeight="bold"
+        name="rateDownload"
+        value={sessionStats.downloadSpeed || 0}
+      />
+      <TorrentFieldFormatter
+        fontSize={'$3'}
+        // fontWeight="bold"
+        name="rateUpload"
+        value={sessionStats.uploadSpeed || 0}
+      />
+    </XStack>
   )
 }
