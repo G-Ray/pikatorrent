58f32ac25162c02f4804515bd9277bd85ddf1d48
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2023-12-13 15:48:28 +0100

    v0.9.0

--- apps/app/app.config.js
+++ apps/app/app.config.js
@@ -23,7 +23,7 @@ module.exports = () => {
       },
       android: {
         package: IS_DEV ? 'com.gray.pikatorrent.dev' : 'com.gray.pikatorrent',
-        versionCode: 16,
+        versionCode: 17,
         adaptiveIcon: {
           foregroundImage: './assets/adaptive-icon.png',
           backgroundColor: '#ffffff',
