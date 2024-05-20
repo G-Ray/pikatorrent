c305c8940eb28d7faeadef6183918d82b1571b2b
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2024-05-20 16:35:58 +0200

    Fix app entry point

--- apps/app/app/index.ts
+++ apps/app/app/index.ts
@@ -2,13 +2,14 @@ import 'expo-router/entry'
 import { polyfillWebCrypto } from 'expo-standard-web-crypto'
 import { Platform } from 'react-native'
 
-import './i18n'
-import { initApp } from './lib/lifecycle'
+import '../i18n'
+import { initApp } from '../lib/lifecycle'
 
 if (Platform.OS === 'web') {
   require('@tamagui/core/reset.css')
 }
 
+console.log('initApp')
 initApp()
 
 polyfillWebCrypto()
