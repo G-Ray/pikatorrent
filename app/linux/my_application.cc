3f9dcb16cb212b289c410eb1c7f2ce72192a85d9
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2024-12-03 16:49:59 +0100

    Capitalize linux title bar

--- app/linux/my_application.cc
+++ app/linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "pikatorrent");
+    gtk_header_bar_set_title(header_bar, "PikaTorrent");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "pikatorrent");
+    gtk_window_set_title(window, "PikaTorrent");
   }
 
     GdkGeometry geometry_min;
