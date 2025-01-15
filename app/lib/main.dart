0e5b26689d44919102429d6e5a83d15f4c48e57f
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2025-01-15 18:18:19 +0100

    fix: check isPreventClose to avoid crash when quitting

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -139,9 +139,12 @@ class _PikaTorrentAppState extends State<PikaTorrentApp> with WindowListener {
 
   @override
   void onWindowClose() async {
-    var appModel = Provider.of<AppModel>(context, listen: false);
-    appModel.setQuitting(true);
-    appModel.quit();
+    bool isPreventClose = await windowManager.isPreventClose();
+    if (isPreventClose) {
+      var appModel = Provider.of<AppModel>(context, listen: false);
+      appModel.setQuitting(true);
+      appModel.quit();
+    }
   }
 
   // App root
