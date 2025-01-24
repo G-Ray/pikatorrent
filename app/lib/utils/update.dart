588a0fb9c90fab25e3b40896e4165036f37ff568
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2025-01-24 19:52:17 +0100

    checkForUpdate: Use /latest api route

--- app/lib/utils/update.dart
+++ app/lib/utils/update.dart
@@ -5,7 +5,7 @@ import 'package:pub_semver/pub_semver.dart';
 import 'package:http/http.dart' as http;
 
 const String _githubApiUrl =
-    'https://api.github.com/repos/G-Ray/pikatorrent/releases';
+    'https://api.github.com/repos/G-Ray/pikatorrent/releases/latest';
 
 // Returns the latest update version, or null
 Future<String?> checkForUpdate(String version) async {
@@ -15,9 +15,9 @@ Future<String?> checkForUpdate(String version) async {
     final response = await http.get(Uri.parse(_githubApiUrl));
 
     if (response.statusCode == 200) {
-      final List<dynamic> data = jsonDecode(response.body);
+      final dynamic data = jsonDecode(response.body);
       // Remove leading 'v' from tag name
-      final String latestVersionString = data.first['tag_name'].substring(1);
+      final String latestVersionString = data['tag_name'].substring(1);
 
       final latestVersion = Version.parse(latestVersionString);
       final currentVersion = Version.parse(version);
