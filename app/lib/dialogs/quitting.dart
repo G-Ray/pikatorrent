febceaa557417500a38cc3be64d0beba421a9be9
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2025-03-19 19:35:26 +0100

    feat: add force quit button & add process indicator

--- app/lib/dialogs/quitting.dart
+++ app/lib/dialogs/quitting.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
 class QuittingDialog extends StatelessWidget {
   const QuittingDialog({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const AlertDialog(
-      title: Text('Quitting'),
-      content: Text('Please wait while PikaTorrent is stopping...'),
+    return AlertDialog(
+      title: const Text('Quitting'),
+      content: const Column(mainAxisSize: MainAxisSize.min, children: [
+        Text('Please wait while PikaTorrent is stopping...'),
+        CircularProgressIndicator()
+      ]),
+      actions: [
+        TextButton(
+          child: const Text('Force quit'),
+          onPressed: () {
+            windowManager.destroy();
+          },
+        ),
+      ],
     );
   }
 }
