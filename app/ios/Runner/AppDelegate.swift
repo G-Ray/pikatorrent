c1ba321737e69a7aa6ae4323b4a14b208008253e
G-Ray <geoffrey.bonneville@gmail.com>
2025-04-19 17:48:39 +0200

    feat(ios): opt-out of backing up Documents/ and Application Support/ directories

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -8,6 +8,21 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+     // Exclude the apps folders from backup.
+    try! setExcludeFromiCloudBackup(isExcluded: true)
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+private func setExcludeFromiCloudBackup(isExcluded: Bool) throws {
+    var values = URLResourceValues()
+    values.isExcludedFromBackup = isExcluded
+
+    var documentsURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+    var applicationSupportDirectoryURL = try! FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+
+    try documentsURL.setResourceValues(values)
+    try applicationSupportDirectoryURL.setResourceValues(values);
 }
