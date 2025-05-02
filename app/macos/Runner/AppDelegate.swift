233c3f2ba99d394085051cebd2b3502e47badf49
G-Ray <geoffrey.bonneville@gmail.com>
2025-05-02 10:49:14 +0200

    fix(macos): prevent app termination on window close

--- app/macos/Runner/AppDelegate.swift
+++ app/macos/Runner/AppDelegate.swift
@@ -5,7 +5,7 @@ import app_links
 @main
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    return true
+    return false
   }
 
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
