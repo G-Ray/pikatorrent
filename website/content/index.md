40bfe38ebce4259aa3c24a033a50f42ade98a38c
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2026-05-04 21:15:55 +0200

    feat: init jaspr_content

---
+++ website/content/index.md
@@ -0,0 +1,40 @@
+# Welcome to Jaspr Content
+
+This is a sample content file. You can add more files here as needed.
+
+---
+
+All standard **markdown syntax** is supported, including:
+
+**Lists**:
+
+- Item 1
+- Item 2
+  1. Nested item
+  2. Nested item 2
+
+**Blockquotes**:
+
+> This is a blockquote.  
+> It can span multiple lines.
+
+**Code blocks**:
+
+```dart
+void main() {
+  print('Hello, world!');
+}
+```
+
+**Inline code**: Use `print('Hello, world!')` to display a message.
+
+**Links**: [Visit Jaspr](https://jaspr.dev)
+
+**Images**: ![Sample Image](https://placehold.co/600x400)
+
+**Tables**:
+
+| Syntax    | Description |
+|-----------|-------------|
+| Header 1  | Content 1   |
+| Header 2  | Content 2   |
--- website/lib/main.server.options.dart
+++ website/lib/main.server.options.dart
@@ -5,10 +5,6 @@
 // Generated with jaspr_builder
 
 import 'package:jaspr/server.dart';
-import 'package:website/components/counter.dart' as _counter;
-import 'package:website/constants/theme.dart' as _theme;
-import 'package:website/pages/about.dart' as _about;
-import 'package:website/app.dart' as _app;
 
 /// Default [ServerOptions] for use with your Jaspr project.
 ///
@@ -26,13 +22,4 @@ import 'package:website/app.dart' as _app;
 ///   runApp(...);
 /// }
 /// ```
-ServerOptions get defaultServerOptions => ServerOptions(
-  clientId: 'main.client.dart.js',
-  clients: {_app.App: ClientTarget<_app.App>('app')},
-  styles: () => [
-    ..._theme.styles,
-    ..._app.AppState.styles,
-    ..._counter.CounterState.styles,
-    ..._about.About.styles,
-  ],
-);
+ServerOptions get defaultServerOptions => ServerOptions(styles: () => []);
