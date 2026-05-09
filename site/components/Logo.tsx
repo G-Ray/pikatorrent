968fd47001db0a48083e1674fbe6ecd8190f7d11
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2026-05-09 19:20:25 +0200

    feat: remove old nextjs site

--- site/components/Logo.tsx
+++
@@ -1,6 +0,0 @@
-import React, { useEffect, useState } from "react";
-
-export const Logo = ({ width = 96 }) => {
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img src="/logo.svg" alt="pikatorrent logo" width={width} />;
-};
